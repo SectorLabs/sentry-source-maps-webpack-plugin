f148a9f1ee42c34ee3b2723cd1c64d2febbb542e
Swen Kooij <swen@sectorlabs.ro>
2017-12-30 16:33:25 +0200

    Make source mapping association work

--- src/index.js
+++ src/index.js
@@ -14,22 +14,24 @@ class Sentry {
     /**
      * Makes a POST request at the specified path.
      */
-    request(path, body, contentType = 'application/json') {
+    request(path, body, headers = {}) {
         let url = `https://sentry.io/api/0/projects/${this.options.org}/${
             this.options.project
         }`;
         url += path;
 
-        const init = {
+        const requestHeaders = {
+            Authorization: `Bearer ${this.options.authToken}`,
+            'content-type': 'application/json',
+        };
+
+        const requestOptions = {
             method: 'POST',
-            headers: {
-                Authorization: `Bearer ${this.options.authToken}`,
-                'Content-Type': contentType,
-            },
+            headers: Object.assign({}, requestHeaders, headers || {}),
             body: body,
         };
 
-        return fetch(url, init).then(response => response.json());
+        return fetch(url, requestOptions).then(response => response.json());
     }
 
     /**
@@ -40,15 +42,30 @@ class Sentry {
     }
 
     /**
-     * Uploads a source map.
+     * Uploads a file as part of a release.
      */
-    uploadSourceMap(version, filePath, name) {
+    uploadFile(version, filePath, name) {
         const form = new FormData();
         form.append('file', fs.createReadStream(filePath));
         form.append('name', name);
 
-        const contentType = form.getHeaders()['content-type'];
-        return this.request(`/releases/${version}/files/`, form, contentType);
+        // create a link to the source map if this is not a source map itself
+        if (!name.endsWith('.map')) {
+            form.append('header', `Sourcemap:${name}.map`);
+        }
+
+        return this.request(
+            `/releases/${version}/files/`,
+            form,
+            form.getHeaders(),
+        );
+    }
+
+    /**
+     * Finalizes a release with the specified version number.
+     */
+    finalizeRelease(version) {
+        return this.request(`/releases/${version}/deploy`, JSON.stringify({}));
     }
 }
 
@@ -66,33 +83,41 @@ function SentrySourceMapPlugin(options) {
  */
 SentrySourceMapPlugin.prototype.apply = function(compiler) {
     compiler.plugin('after-emit', (compilation, callback) => {
-        // iterate over all chunks and find all source maps that
+        // iterate over all chunks and find all JS files that
         // were emitted during the build
-        const sourceMaps = {};
+        const files = {};
         compilation.chunks.forEach(chunk => {
             chunk.files
-                .filter(fileName => fileName.endsWith('.map'))
+                .filter(
+                    fileName =>
+                        fileName.endsWith('.js') || fileName.endsWith('.map'),
+                )
                 .forEach(fileName => {
-                    sourceMaps[fileName] = compilation.assets[fileName];
+                    files[fileName] = compilation.assets[fileName];
                 });
         });
+
+        const publicPath = compilation.options.output.publicPath;
 
         this.sentry
             // create a new release
             .createRelease(this.options.version)
 
-            // upload all the source maps we found
+            // upload all the files we found
             .then(() =>
                 Promise.all(
-                    Object.keys(sourceMaps).map(fileName =>
-                        this.sentry.uploadSourceMap(
+                    Object.keys(files).map(fileName =>
+                        this.sentry.uploadFile(
                             this.options.version,
-                            sourceMaps[fileName].existsAt,
-                            fileName, // TODO: maybe combine this with publicPath
+                            files[fileName].existsAt,
+                            `~${path.join(publicPath, fileName)}`,
                         ),
                     ),
                 ),
             )
+
+            // finalize the release
+            .then(() => this.sentry.finalizeRelease(this.options.version))
 
             // signal that we uploaded all the source maps
             .then(() => callback())
@@ -104,8 +129,6 @@ SentrySourceMapPlugin.prototype.apply = function(compiler) {
                 );
                 return callback();
             });
-
-        // const publicPath = compilation.options.output.publicPath
     });
 };
 
